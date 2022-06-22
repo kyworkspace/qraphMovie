51604d8c96816be6057d0a1845b0c33be145d442
kyworkspace <ky91zzang@naver.com>
2022-06-22 23:24:21 +0900

    resolve & Mutation resolver

--- README.md
+++ README.md
@@ -66,7 +66,28 @@ mutation Mutation($text: String, $userId: ID) {
  #### ! (느낌표) 사용
   - required 표현
   - Not Null과 같은 표현이다.
+  - 사용하지 않는다면 해당 필드는 nullable과 같은 표현이다.
   ```
   allTweets : [Tweet!]!
   ```
   - 무조건 Tweet에 의한 배열을 리턴한다는 뜻이다.
+  - [null]! 은 에러를 발생한다.
+
+
+  #### resolvers
+ - apollo에서 def를 통해 쿼리를 실행시키면 resolver의 동일한 이름의 데이터(혹은 메서드) 로 연결됨
+ - resolvers 함수는 데이터베이스에 액세스한 다음 데이터를 반환.
+ - resolvers 에서 함수가 실행될때 첫번째 argument는 root argument 이다. 실제로 사용자가 입력한 argument는 두번쨰 부터 들어간다.
+ -  두번째 인자가 mutaion에서 보낸 인자이다.
+ ```
+     Query:{
+        allTweets(){
+            return tweets;
+        },
+        tweet(root,{id}){
+            return tweets.find(tweet=>tweet.id === id);
+        }
+
+    }
+ ```
+  * 위에서 {id} 부분이 typeDefs에서 정의한 ID가 들어가는 곳이다.
--- server.js
+++ server.js
@@ -3,35 +3,77 @@
 import {ApolloServer,gql} from 'apollo-server';
 
 
+//graphql 의 법칙을 따라야함
+let tweets = [
+    {
+        id : "1",
+        text : "first",
+    },
+    {
+        id : "2",
+        text : "second",
+    }
+]
+
 //SDL ==> Schema Definition Language
 const typeDefs = gql`
-    type User{
-        id : ID!
-        username : String!
-        firstName : String
-        lastName : String
-    }
-
-    type Tweet {
-        id : ID!
-        text : String!
-        author : User!
-    }
-
-    type Query{
-        allTweets : [Tweet!]!
-        Tweet(id:ID!) : Tweet
-    }
-
-    type Mutation{
-        postTweet(text:String!, userId : ID!) : Tweet!
-        deleteTweet(id:ID!) : Boolean!
-    }
+type User {
+    id: ID!
+    username: String!
+    firstName: String!
+    lastName: String
+  }
+  type Tweet {
+    id: ID!
+    text: String!
+  }
+  type Query {
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+    ping:String!
+    postTweet(text: String!, userId: ID!): Tweet!
+  }
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
+  }
 `;
 // 배열 형태의 Tweet 리턴
 // 1개의 Tweet 사용 ==> argument 필요 ==> id를 받아서 표현
 
-const server = new ApolloServer({typeDefs})
+
+const resolvers={
+    Query:{
+        allTweets(){
+            return tweets;
+        },
+        tweet(root,{id}){
+            return tweets.find(tweet=>tweet.id === id);
+        }
+
+    },
+    Mutation:{
+        //postTweet(text: String!, userId: ID!): Tweet!
+        postTweet(_,{text,userId}){
+            const newTweet={
+                id : tweets.length+1,
+                text : text,
+            }
+            tweets.push(newTweet);
+            return newTweet;
+        },
+        deleteTweet(_,{id}){
+            // Boolean!
+            const tweet = tweets.find(tweet=>tweet.id === id);
+            if(!tweet) return false;
+            tweets = tweets.filter(tweet=>tweet.id !== id)
+            return true;
+        }
+    }
+
+}
+
+const server = new ApolloServer({typeDefs,resolvers})
 
 server.listen().then(({url})=>{
     console.log(`Running on ${url}`)
